c9c7a9cf554c68e1ee364e4b5c63e2e467888c7e
hsin <joanne45644@gmail.com>
2025-04-17 17:30:03 +0800

    fixed:vite config.js build

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/GSAP-3.js_scroll_animation/',
+
+  build: {
+    minify: 'esbuild',
+    target: 'es2015',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  },
 });
