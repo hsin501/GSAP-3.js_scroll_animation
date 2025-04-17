aa9ed8de0fdd506751111c543dac7db4775500f9
hsin <joanne45644@gmail.com>
2025-04-17 17:55:02 +0800

    fix: Reconfigure vite.config.js to remove console.log fix: Add favicon to the webpage

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,13 @@ export default defineConfig({
   base: '/GSAP-3.js_scroll_animation/',
 
   build: {
+    minify: 'terser',
+    target: 'es2015',
     terserOptions: {
       compress: {
         drop_console: true,
         drop_debugger: true,
+        pure_funcs: ['console.log', 'console.info', 'console.debug'], // ✅ 雙重保險
       },
     },
   },
